0757dc0aadb57acc31da1960452a2e5649a26468
omni-axa <kgoon007@gmail.com>
2012-10-16 13:33:19 +0530

    further refined inventory overhauler for full inventories, yay Chasington

--- setup-lolfixer_ONLY_DEBUG_MODE.bat
+++ setup-lolfixer_ONLY_DEBUG_MODE.bat
@@ -1,2 +1,2 @@
 setup-lolfixer.exe --force-install-list 0 1 2 3 4 5 6 7 8 9 10 11 12 > lolfixer.log
-setup-lolfixer.exe --force-uninstall-list 0 1 2 3 4 5 6 7 8 9 10 11 12 > uninstall_lolfixer.log
+setup-lolfixer.exe --uninstall > uninstall_lolfixer.log
